77cac62bc2fb1a4643290f6227b3f47363bf2dbe
emiliamassing <emiliamassings@gmail.com>
2023-02-22 18:00:36 +0100

    Changes to nav, started to add ts for menu

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,1 +1,9 @@
+const menuButton = document.querySelector('.toggleMenu') as HTMLButtonElement;
+const menuUl = document.querySelector('.menuLinks') as HTMLUListElement;
+let menuOpen: boolean = false;
 
+menuButton.addEventListener('click', toggleMenu);
+
+function toggleMenu(): void {
+    
+}
