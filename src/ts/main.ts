580b2cbf59719a5370427b449dd1cfcfb7e3ed4b
emiliamassing <emiliamassings@gmail.com>
2023-02-23 08:24:33 +0100

    small adjustments for menu

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -2,17 +2,30 @@ const menuButton = document.querySelector('.toggleMenu') as HTMLButtonElement;
 const menuUl = document.querySelector('.menuLinks') as HTMLUListElement;
 let menuOpen: boolean = false;
 
+menuButton.addEventListener('click', toggleMenu);
+    const listItemLinks = document.querySelectorAll('.listItem');
+    listItemLinks.forEach((link) => {
+    link.addEventListener('click', closeMenu);
+});
+
 function toggleMenu(): void {
     menuOpen = !menuOpen; //Gör att menyn får värdet true
-  if (!menuOpen) {
+    if (!menuOpen) {
     menuButton.blur(); // Tar bort fokus från knappen
-  }
-  if (menuOpen) {
+    }
+    if (menuOpen) {
     menuButton.classList.add('active');
     menuUl.classList.add('active');
-  } else {
+    } else {
     menuButton.classList.remove('active');
     menuUl.classList.remove('active');
-  }
-  menuButton.setAttribute('aria-expanded', 'true');
+    }
+    menuButton.setAttribute('aria-expanded', 'true');
 };
+
+function closeMenu(): void {
+    menuOpen = false;
+    menuButton.blur();
+    menuButton.classList.remove('active');
+    menuButton.setAttribute('aria-expanded', 'false');
+}
