0573c661142dfec33830b20c2f4eb2d29f1dec78
emiliamassing <emiliamassings@gmail.com>
2023-02-23 08:20:19 +0100

    Added function for mobile menu

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -2,8 +2,17 @@ const menuButton = document.querySelector('.toggleMenu') as HTMLButtonElement;
 const menuUl = document.querySelector('.menuLinks') as HTMLUListElement;
 let menuOpen: boolean = false;
 
-menuButton.addEventListener('click', toggleMenu);
-
 function toggleMenu(): void {
-    
-}
+    menuOpen = !menuOpen; //Gör att menyn får värdet true
+  if (!menuOpen) {
+    menuButton.blur(); // Tar bort fokus från knappen
+  }
+  if (menuOpen) {
+    menuButton.classList.add('active');
+    menuUl.classList.add('active');
+  } else {
+    menuButton.classList.remove('active');
+    menuUl.classList.remove('active');
+  }
+  menuButton.setAttribute('aria-expanded', 'true');
+};
